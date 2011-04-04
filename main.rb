4a6ae7e48d6eead39db35e2bd848d4c57e9fe5b9
Rafael Jegundo <rafael.jegundo@gmail.com>
2011-04-04 20:13:05 +0100

    Better Skeleton

--- main.rb
+++ main.rb
@@ -12,18 +12,26 @@ Upgrades ao problema (pontos a adicionar resolvida a primeira aproximação):
 3)	Electrões em vez de iões a serem lançados e uma secção eficaz mais realista que terá de ser tratada pelo métodos numéricos aprendidos.
 """
 
+$c = 299792458
+i = 0
+a = 2
+Energia_limiar = 1E3
+Step = 1
+
 class Ion
   attr_accessor :x, :y, :energy, :vx, :vy
   def initialize
-  @x = 0 # metros
-  @y = 0 # metros
-  @vx = 10 # m/s
-  @vy = 0 # m/s
-  @energy = 1E9 # eV
+    @mass = 931.46E6/($c**2)
+    @x = 0 # metros
+    @y = 0 # metros
+    @vy = 0 # c
+    @vx = Math.sqrt(1E9*2*@mass) # c
+    @energy = 1E9 # eV
   end
   def colides
-    @y += 5
-    @energy = @energy/100 
+    #TBD - Placeholder for the collision aftermath
+    @energy = @energy - @energy*0.1
+    @vx -= @vx*0.1  
   end
   def inspect
     "The ion is at #{@x}, #{@y} with an energy of #{@energy} and a velocity of #{@vx}, #{@vy}"
@@ -33,20 +41,19 @@ class Ion
     rescue
     $data = [[@x, @y, @energy, @vx, @vy]]
   end
+  def findAr
+    #TBD - Placeholder for the collision probability
+    rand >0.80
+  end
 end
-
-i = 0
-a = 2
-Energia_limiar = 1E3
-Step = 10
-
+      
 while i<a
   subject = Ion.new
   p subject
   while subject.energy > Energia_limiar
     subject.x = subject.vx*Step + subject.x
     subject.y = subject.vy*Step + subject.y
-    if rand > 0.95
+    if subject.findAr
       subject.colides
       puts 'HIT'
     end
