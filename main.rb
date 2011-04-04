063734587a5050a57b568e08afc622822af00f6f
Rafael Jegundo <rafael.jegundo@gmail.com>
2011-04-04 18:56:28 +0100

    basic skeleton

--- main.rb
+++ main.rb
@@ -12,6 +12,51 @@ Upgrades ao problema (pontos a adicionar resolvida a primeira aproximação):
 3)	Electrões em vez de iões a serem lançados e uma secção eficaz mais realista que terá de ser tratada pelo métodos numéricos aprendidos.
 """
 
-EcI = 1E9
+class Ion
+  attr_accessor :x, :y, :energy, :vx, :vy
+  def initialize
+  @x = 0 # metros
+  @y = 0 # metros
+  @vx = 10 # m/s
+  @vy = 0 # m/s
+  @energy = 1E9 # eV
+  end
+  def colides
+    @y += 5
+    @energy = @energy/100 
+  end
+  def inspect
+    "The ion is at #{@x}, #{@y} with an energy of #{@energy} and a velocity of #{@vx}, #{@vy}"
+  end
+  def register
+    $data << [@x, @y, @energy, @vx, @vy]
+    rescue
+    $data = [[@x, @y, @energy, @vx, @vy]]
+  end
+end
 
-Ecf = 1E3
+i = 0
+a = 2
+Energia_limiar = 1E3
+Step = 10
+
+while i<a
+  subject = Ion.new
+  p subject
+  while subject.energy > Energia_limiar
+    subject.x = subject.vx*Step + subject.x
+    subject.y = subject.vy*Step + subject.y
+    if rand > 0.95
+      subject.colides
+      puts 'HIT'
+    end
+    p subject
+  end
+  subject.register
+  i += 1
+end
+
+#output data
+
+$data.each{|a| puts "#{a[0]}, #{a[1]}, #{a[2]}, #{a[3]}, #{a[4]}"}
+
