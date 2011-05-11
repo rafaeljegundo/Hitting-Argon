137b7dc15002f955f8a85edd7fc6ecbad2df3bf3
Rafael Jegundo <rafael.jegundo@gmail.com>
2011-05-11 19:11:04 +0100

    tridiagonal function corrected.

--- qsplines.py
+++ qsplines.py
@@ -3,36 +3,32 @@
 """
 qsplines.py
 
-Uses tridiagonal.py from http://www.math-cs.gordon.edu/courses/mat342/python.html
-
 Created by Rafael Jegundo on 2011-05-10.
 Copyright (c) 2011 SMMC. All rights reserved.
 """
 import numpy as np
 import matplotlib
-matplotlib.use('PDF')
+matplotlib.use('macosx')
 
-import matplotlib.pyplot as plt
-from pylab import plot
+import pylab as plt
 
-
-
-def tridiagonal(a,b,c,d):
+def tridiagonal(a,b,c,v):
 	
 	n = len(b)
-
-	c[0]/=b[0]
-	d[0]/=b[0]
-
-	for i in range(2,n):
-	    identity = 1 / (b[i] - c[i-1] * a[i])
-	    c[i] = c[i]* identity
-	    d[i] = (d[i] - d[i-1] * a[i]) * identity
-
-	x = d[:]
 	
-	for i in range(-1,n-1):
-	    x[i] = d[i]-c[i]*x[i+1]
+	for i in range(1,n-1):
+	    m = a[i]/b[i-1]
+	    b[i] -= m*c[i-1]
+	    v[i] -= m*v[i-1]
+
+	x = np.zeros(n)
+	x[-1] = v[-1]/b[-1]
+
+	print x,n
+	print len(x), len(v), len(c), len(b)
+	
+	for i in range(n-2,-1,-1):
+	    x[i] = (v[i]-c[i]*x[i+1])/b[i]
 		
 	return x
 
@@ -46,7 +42,7 @@ def splinefun(x,y,f,h,xr):
 	
 	for p in range(pontos):
 			for a in range(n-1):
-				if x[a] < xr[p] and x[a+1] > xr[p]:
+				if x[a] <= xr[p] and x[a+1] >= xr[p]:
 					print a,p, x[a], xr[p], x[a+1]
 					yr.append( \
 						(f[a]/(6*h[a]))*(x[a+1]-xr[p])**3 +\
@@ -99,19 +95,14 @@ def main():
 		v.append(((6/h[i])*(y[i+1]-y[i]))-(6/(h[i-1]))*(y[i]-y[i-1]))
 		i += 1
 	
+	xr = np.linspace(1E-4,50,100)	
 	
-	a = c = h[:] # len n-2
-	b = u[:] # len n-1
-	d = v[:]
-	xr = np.linspace(8E-5,50,100)
-	
-	
-	f = tridiagonal(a,b,c,d)
+	f = tridiagonal(h,u,h,v)
 	
 	yr = splinefun(x,y,f,h,xr)
 	
 	r = open('results.txt','w')
-		
+
 	# writing to file	
 	i = 0
 	while True:
@@ -122,9 +113,13 @@ def main():
 			break
 			
 	r.close()
-
-	plt.plot(x,y)
+		
+	# Another hack. The problem is on yr calculus.
+	print len(x), len(y)
+	print len(xr[:88]), len(yr)
+	plt.plot(x,y,'ro',xr[:88],yr,'bo')
 	plt.show()
+	
 	pass
 
 
