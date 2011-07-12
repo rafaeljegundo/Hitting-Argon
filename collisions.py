ebef00e99014959bdd77643d77a92152dab69b87
Rafael Jegundo <rafael.jegundo@gmail.com>
2011-07-12 10:59:15 +0100

    refactored and expanded

--- collisions.py
+++ collisions.py
@@ -1,51 +1,21 @@
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-collisions2D.py
-
-Created by Rafael Jegundo on 2011-05-11.
-Copyright (c) 2011 SMMC. All rights reserved.
-
-Notes: the step needs to be very small, due to the small free mean path. Final results for traveled distance should be ~1E-6
-
-Enunciado: 
-
-Energia cinética inicial ~1MeV. Iões lançados forward.
-
-Depois de cada colisão há isotropia (temos de considerar conservação de momento linear e de energia –
-recuo do átomo com que colide, que inicialmente está em repouso)
-
-Densidade do meio – N átomos /cm3 (number density à pressão atmosférica)
-
-A simulação para quando os iões têm energia final de ~1keV. Regista-se a posição final do ião.
-
-Iões de Ar+ em atmosfera de Ar.
-
-Usar como secção eficaz o modelo da esfera rígida (para determinar o caminho livre médio do ião)
-
-Upgrades ao problema (pontos a adicionar resolvida a primeira aproximação):
-
-1) Os átomos do meio têm distribuição de velocidade normal com média em 3/2kT. - DONE
-2) Há um campo eléctrico uniforme aplicado.
-3) Electrões em vez de iões a serem lançados e uma secção eficaz mais realista que terá de ser
-tratada pelo métodos numéricos aprendidos.
-4) Passar para 3D.
-
-Test and END.
-"""
 
 from random import random
 from numpy import pi, sin, cos, sqrt, hypot, arctan
-import sys
+from sys import exit
 from time import time
 
 
 # Constantes Universais
 
-c = 299792458
+c = 299792458 # m/s
 K = 8.61E-5 # eV/K
-T = 293
-q = 1.60217646E19 # Coulomb
+T = 293 # K
+argonMass = 931.46E6/(c**2) # eV
+electronMass = 511E3/(c**2) # eV
+n = 2.18E19 # àtomos /cm^3
+
 
 class Particle:	
 	
@@ -53,113 +23,140 @@ class Particle:
 	
 	def __init__(self, particle_type, energy, x, y, direction):
 		
-		argonMass = 931.46E6/(c**2)
-		electronMass = 9.1093821E-31
+		self.collisioncounter = 0	
 		
 		if particle_type == "Argon+":		
 			self.mass = argonMass
 		elif particle_type == "Electron":
-			print "electron!"
 			self.mass = electronMass
 		
 		self.particle_type = particle_type
+
 		self.x = x
 		self.y = y
+
 		self.energy = energy
+
 		self.v = sqrt((2*energy)/self.mass)
 		self.vx = self.v*cos(direction)
 		self.vy = self.v*sin(direction)
 				
-		self.collisioncounter = 0	
-		self.state = ""
-		
 		return
 
-	def colides(self, collision_type="2D-inerte gas"):
+	def colides(self, collision_type="3/2KT Energy gas", energy_method="f(v)"):
 
 		self.collisioncounter += 1
+		
 		self.vi1 = hypot(self.vx,self.vy)
 		
-		if collision_type == "2D-inerte gas":
-			# Válido para electrões ????????????????????????
+		if self.particle_type == "Argon+":
+			
 			teta = 2*pi*random()
 			teta1 = arctan(sin(teta)/(1+cos(teta)))
 			teta2 = 0.5*(pi-teta)
-
+			
+		elif self.particle_type == "Electron":
+			
+			teta = 2*pi*random()
+			teta1 = arctan(sin(teta)/((electronMass/argonMass)+cos(teta)))
+			teta2 = 0.5*(pi-teta)
+		
+		# Exit velocity calculation for each collision type
+		if collision_type == "0 Energy gas":
+ 
 			self.vf1 = self.vi1/sqrt(1+((sin(teta1)**2)/(sin(teta2)**2)))
 
-		elif collision_type == "2D-3/2KTEnergy gas":
+		elif collision_type == "3/2KT Energy gas":
 			
-			teta = 2*pi*random()
-			teta1 = arctan(sin(teta)/(1+cos(teta)))
-			teta2 = 0.5*(pi-teta)
-			
-			direction = random()*180
+			direction = random()*pi
 
 			gas = Particle("Argon+", (3/2)*K*T, self.x, self.y, direction)
 			
 			vi2 = hypot(gas.vx, gas.vy)		
 
 			a = 1 + (sin(teta1)/sin(teta2))**2 
-			b = 2*vi2*sin(teta1)/sin(teta2)**2*sin(teta) # Será teta? 
-			c = -self.vi1**2 + vi2**2 *(-1 + ( sin(teta)/sin(teta2)))
+			b = 2*vi2*sin(teta1)/sin(teta2)**2*sin(teta) 
+			c = -self.vi1**2 - (vi2**2)*(-1 + ( sin(teta)/sin(teta2)))
 			
 			self.vf1 = (-b + sqrt(b*b-4*a*c))/(2*a)
-			
-		elif collision_type == "3D-3/2KTEnergy gas":
-			
-			print "TBI"
 			
 		else:
 			
 			print "unknown collision type"
-			sys.exit()
+			exit()
 		
+		# Final velocity components calculations
 		self.vx = self.vf1*cos(teta1)
 		self.vy = self.vf1*sin(teta1)
-		self.energy = 0.5*self.mass*self.vf1*self.vf1
-
-		return
-		
-	def log(self):
-		self.state = map(str,[self.x, self.y, self.energy, self.vx, self.vy, self.collisioncounter])
+				
+		# Final energy calculations
+		if energy_method == "f(v)":
+			self.energy = 0.5*self.mass*self.vf1*self.vf1
+ 		elif energy_method == "half":
+			self.energy = 0.5*self.energy
+		else:
+			print "unknown energy calculation method"
+			exit()
 		return
 
 def ionTrip2D(subject, step, la, f, eField = (0.0,0.0)):
+		
+	if subject.mass == electronMass:
+		charge = -1.60217646E19
+	else:
+		charge = 1.60217646E19
 	
-	acceleration = (q*eField[0]/subject.mass,q*eField[1]/subject.mass)	
+	acceleration = (charge*eField[0]/subject.mass,charge*eField[1]/subject.mass)
+	
+	lastpositionx  = subject.x  
+	lastpositiony  = subject.y
 	
 	while True:
-		lastpositionx  = subject.x  
-		lastpositiony  = subject.y
 		subject.vx += acceleration[0]*step
 		subject.vy += acceleration[1]*step  
-		subject.x = subject.vx*step + subject.x 
-		subject.y = subject.vy*step + subject.y
+		subject.x += subject.vx*step
+		subject.y += subject.vy*step
 		distpercorrida = sqrt((subject.x-lastpositionx)**2 + (subject.y-lastpositiony)**2)
 		
 		# Collision condition: when distance traveled converges to la, collision probability converges to 1.
-		# Deverá evoluir para colisão nula
 		
-		if random() < distpercorrida/la*0.5 :
-			subject.colides()	
-			subject.log()
-		if subject.energy < 1E3:
-			f.write("%s\t%s\t%s\t%s\t%s\n" % (subject.x, subject.y, hypot(subject.x,subject.y), subject.energy, subject.collisioncounter))
-			return
+		if subject.particle_type == "Electron":
+			
+			if random() < (distpercorrida/la(subject.energy))*0.5 :
+				subject.colides()	# Change collisions conditions here
+				lastpositionx = subject.x  
+				lastpositiony = subject.y
+				
+			if subject.energy < 1E3:
+				f.write("%s\t%s\t%s\t%s\t%s\n" % (subject.x, subject.y, hypot(subject.x,subject.y), subject.energy, subject.collisioncounter))
+				return
+
+		elif subject.particle_type == "Argon+":
+
+			if random() < (distpercorrida/la)*0.5 :
+				subject.colides("0 Energy gas", "half") 	# Change collisions conditions here
+				lastpositionx = subject.x  
+				lastpositiony = subject.y
+				
+			if subject.energy < 1E3:
+				f.write("%s\t%s\t%s\t%s\t%s\n" % (subject.x, subject.y, hypot(subject.x,subject.y), subject.energy, subject.collisioncounter))
+				return
+		else:
+			print "panic"
+	return
 			
 	
 def simulate_collisions2D(step, free_mean_path,electric_field, ions, particle_type):
 
 	# Some Default values
-	energy = 1E9
+	energy = 1E9 #eV
 	x_initial = 0
 	y_initial = 0
-	direction = 0 # Degrees between ion direction and the positive X axe.
+	direction = 0 # Degrees between ion/electron direction and the positive X axe.
 	
 	f = file('collisionslog2D.txt','w')
 	
-	f.write("%s\t%s\t%s\t%s\t%s\n" % ("x", "y", "distance", "energy", "collision"))
+	f.write("%s\t%s\t%s\t%s\t%s\t%s\n" % ("#", "x", "y", "distance", "energy", "collision"))
 
 	i = 0	
 	while i < ions:
@@ -172,12 +169,7 @@ def simulate_collisions2D(step, free_mean_path,electric_field, ions, particle_ty
 	pass
 	
 
-
-
 if __name__ == '__main__':
-	print u"Falta a probabilidade de colisão nula"
-
-	print u"Note-se que no caso de electrões, e = -q"
 	
 	# The options are "Argon+" and "Electron"
 	particle_type = "Argon+"
@@ -186,14 +178,41 @@ if __name__ == '__main__':
 	electric_field = (0,0) 
 
 	# Measure of effective section
-	free_mean_path = 2.36E-9 
+	cross_section = []
+	f = open('results.txt', 'r')
 	
-	# Must be very small to be reasonable, according to the free mean path
-	step = 0.00000000001
+	for line in f:
+		cross_section.append(line[:-1].split('\t'))
+
+	cross_section = map(lambda x: (float(x[0]), float(x[1])),cross_section)
+	
+	if particle_type == "Electron":
+
+		def free_mean_path(energy):
+		
+			if energy < cross_section[-1][0]:
+				for pair in cross_section:
+					if (pair[0]-energy) < 1E-2:
+						sigma = pair[1]
+			else:
+				a = 1.25E-14
+				b = 6.72E-1
+				sigma = a/(energy**(-b))
+			
+			return 0.5/(sigma*n)
+
+	else:
+		
+		free_mean_path = 2.36E-9 # m
+		
+	# Must be << free_mean_path
+	step = 1E-15
 	
 	# Number of ions to be launched
-	ions = 20
-
+	ions = 1000
+			
 	print "2D Simulation"
-	simulate_collisions2D(step, free_mean_path, electric_field, ions, particle_type)
+	print "the collision calculations only work for two bodies with the same mass. To be corrected, for the sake of electrons."
+	import profile
+	profile.run('simulate_collisions2D(step, free_mean_path, electric_field, ions, particle_type)')
 	
