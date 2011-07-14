a15878136e4cb8f3679d577d579f93ed7b4d11ce
Rafael Jegundo <rafael.jegundo@gmail.com>
2011-07-14 15:48:52 +0100

    kinda final version

--- collisions3D.py
+++ collisions3D.py
@@ -3,17 +3,17 @@
 
 from random import random
 from numpy import pi, sin, cos, sqrt, hypot, arctan
-import sys
 from time import time
 
 
 # Constantes Universais
 
-c = 299792458
+c = 299792458 # m/s
 K = 8.61E-5 # eV/K
-T = 293
-q = 1.60217646E19 # Coulomb
-
+T = 293 # K
+argonMass = 931.46E6/(c**2) # eV
+electronMass = 511E3/(c**2) # eV
+n = 2.18E25 # àtomos /m^3
 
 class Particle:	
 	
@@ -21,21 +21,15 @@ class Particle:
 	
 	def __init__(self, particle_type, energy, x, y, z, vx, vy, vz):
 		
-		
 		self.teta = arctan(vy/vx)
-		
 		if vz == 0:
 			self.phi = 0	
 		else:
 			self.phi = arctan(hypot(vx,vy)/vz)
 		
-		argonMass = 931.46E6/(c**2)
-		electronMass = 9.1093821E-31
-		
 		if particle_type == "Argon+":		
 			self.mass = argonMass
 		elif particle_type == "Electron":
-			print "electron!"
 			self.mass = electronMass
 		
 		self.particle_type = particle_type
@@ -57,16 +51,13 @@ class Particle:
 		self.vz = self.v*s
 				
 		self.collisioncounter = 0	
-		self.state = ""
-		
+				
 		return
 
 	def colides(self):
-
+		
 		self.collisioncounter += 1
-		
-		#self.v = sqrt(self.vx**2 + self.vy**2 + self.vz**2)
-		
+				
 		# Generate target atom of the gas
 		vxg = random()
 		vyg = random()
@@ -77,7 +68,7 @@ class Particle:
 		#u_gas = sin(gas_atom.phi)*sin(gas_atom.phi)
 		#s_gas = cos(gas_atom.phi)
 		
-		# Conversão para o sistema do centro de massa ?
+		# Conversão para o sistema do centro de massa
 		self.vfx = self.vx - gas_atom.vx
 		self.vfy = self.vy - gas_atom.vy
 		self.vfz = self.vz - gas_atom.vz
@@ -89,19 +80,31 @@ class Particle:
 		s = self.vfz/self.vf
 		
 		# Qual o m e M?
-		gama1 = gas_atom.mass/(self.mass+gas_atom.mass)
-		gama2 = self.mass/(self.mass+gas_atom.mass)
+		gama1 = self.mass/(self.mass+gas_atom.mass)
+		gama2 = gas_atom.mass/(self.mass+gas_atom.mass)
+
 		
-		vcmx = gama2*self.vf*q # mas estes dois ultimos termos == vx !!! 
-		vcmy = gama2*self.vf*u
-		vcmz = gama2*self.vf*s
+		#vcmx = gama2*self.vf*q # mas estes dois ultimos termos == vx !!! 
+		#vcmy = gama2*self.vf*u
+		#vcmz = gama2*self.vf*s
 		
 		ro = sqrt(1-s**2)
 		
-		# Cosenos directores '
-		ql = (sin(self.phi)*cos(self.teta)*s*q-sin(self.phi)*sin(self.teta)*u)/ (ro + cos(self.phi)*q)
-		ul = (sin(self.phi)*cos(self.teta)*s*u-sin(self.phi)*sin(self.teta)*q)/ (ro + cos(self.phi)*u)
-		sl = -sin(self.phi)*sin(self.teta)*ro + cos(self.phi)*s
+		# 
+		phi = gas_atom.phi
+		teta = gas_atom.teta
+		
+		# Cosenos directores ' - colisao
+#		ql = (sin(self.phi)*cos(self.teta)*s*q-sin(self.phi)*sin(self.teta)*u)/ (ro + cos(self.phi)*q)
+#		ul = (sin(self.phi)*cos(self.teta)*s*u-sin(self.phi)*sin(self.teta)*q)/ (ro + cos(self.phi)*u)
+#		sl = -sin(self.phi)*sin(self.teta)*ro + cos(self.phi)*s
+
+		# OR
+		
+		ql = (sin(phi)*cos(teta)*s*q-sin(phi)*sin(teta)*u)/ (ro + cos(phi)*q)
+		ul = (sin(phi)*cos(teta)*s*u-sin(phi)*sin(teta)*q)/ (ro + cos(phi)*u)
+		sl = -sin(phi)*sin(teta)*ro + cos(phi)*s
+		
 		
 		# velocidade no sistema do CM
 		
@@ -109,31 +112,33 @@ class Particle:
 		vlcmy = gama2*self.vf*ul
 		vlcmz = gama2*self.vf*sl
 		
-		#
 		self.vx = vlcmx+gama1*self.vfx+gama2*gas_atom.vx
 		self.vy = vlcmx+gama2*gas_atom.vy
 		self.vz = vlcmz+gama1*self.vfz+gama2*gas_atom.vz
-
+				
 		self.energy = 0.5*self.mass*self.vf*self.vf
-
+				
 		return
 		
-	def log(self):
-		self.state = map(str,[self.x, self.y, self.energy, self.vx, self.vy, self.collisioncounter])
-		return
-		
-
 def ionTrip3D(subject, step, la, f, eField = (0.0, 0.0, 0.0)):
+	
+	if subject.mass == electronMass:
+		charge = -1.60217646E19
+	else:
+		charge = 1.60217646E19
+		
+	acceleration = (charge*eField[0]/subject.mass,charge*eField[1]/subject.mass, charge*eField[2]/subject.mass)	
 
-	acceleration = (q*eField[0]/subject.mass,q*eField[1]/subject.mass, q*eField[2]/subject.mass)	
-
+	lastpositionx  = subject.x  
+	lastpositiony  = subject.y
+	lastpositionz  = subject.z
+	
 	while True:
-		lastpositionx  = subject.x  
-		lastpositiony  = subject.y
-		lastpositionz  = subject.z
+		
 		subject.vx += acceleration[0]*step
 		subject.vy += acceleration[1]*step  
-		subject.vz += acceleration[2]*step  
+		subject.vz += acceleration[2]*step 
+		 
 		subject.x = subject.vx*step + subject.x 
 		subject.y = subject.vy*step + subject.y
 		subject.z = subject.vz*step + subject.z
@@ -141,13 +146,31 @@ def ionTrip3D(subject, step, la, f, eField = (0.0, 0.0, 0.0)):
 		distpercorrida = sqrt((subject.x-lastpositionx)**2 + (subject.y-lastpositiony)**2 + (subject.z-lastpositionz)**2)
 
 		# Collision condition: when distance traveled converges to la, collision probability converges to 1.
+		if subject.particle_type == "Electron":
+			
+			if random() < (distpercorrida/la(subject.energy))*0.5 :
+				subject.colides()
+				lastpositionx = subject.x  
+				lastpositiony = subject.y
+				lastpositionz  = subject.z
+				
+			if subject.energy < 1E3:
+				f.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (subject.x, subject.y,  subject.z, sqrt((subject.x-lastpositionx)**2 + (subject.y-lastpositiony)**2 + (subject.z-lastpositionz)**2), subject.energy, subject.collisioncounter))
+				return
 
-		if random() < distpercorrida/la*0.5 :
-			subject.colides()	
-			subject.log()
-		if subject.energy < 1E3:
-			f.write("%s\t%s\n" % (subject.energy, subject.collisioncounter))
-			return
+		elif subject.particle_type == "Argon+":
+
+			if random() < (distpercorrida/la)*0.5 :
+				subject.colides() 	# Change collisions conditions here
+				lastpositionx = subject.x  
+				lastpositiony = subject.y
+				lastpositionz = subject.z
+				
+			if subject.energy < 1E3:
+				f.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (subject.x, subject.y, subject.z, sqrt((subject.x)**2 + (subject.y)**2 + (subject.z)**2), subject.energy, subject.collisioncounter))
+				return
+		else:
+			print "panic"
 			
 			
 def simulate_collisions3D(step, free_mean_path,electric_field, ions, particle_type):				
@@ -165,7 +188,7 @@ def simulate_collisions3D(step, free_mean_path,electric_field, ions, particle_ty
 	
 	f = file('collisionslog3D.txt','w')
 	
-#SRSR	f.write("%s\t%s\t%s\t%s\t%s\n" % ("x", "y", "distance", "energy", "collision"))
+	f.write("%s\t%s\t%s\t%s\t%s\t%s\t%s\n" % ("#","x", "y", "z", "distance", "energy", "collision"))
 
 	i = 0	
 	while i < ions:
@@ -175,30 +198,53 @@ def simulate_collisions3D(step, free_mean_path,electric_field, ions, particle_ty
 	
 	f.close()
 	
-	
 	pass
 	
 	
 if __name__ == '__main__':
-	
-	print u"Falta a probabilidade de colisão nula"
-
-	print u"Note-se que no caso de electrões, e = -q"
-	
+		
 	# The options are "Argon+" and "Electron"
-	particle_type = "Argon+"
+	particle_type = "Electron"
 	
 	# Electric field module for x and y directions
 	electric_field = (0,0,0) 
 
 	# Measure of effective section
-	free_mean_path = 2.36E-9 
+	cross_section = []
+	f = open('results.txt', 'r')
 	
+	for line in f:
+		cross_section.append(line[:-1].split('\t'))
+
+	cross_section = map(lambda x: (float(x[0]), float(x[1])),cross_section)
+	
+	if particle_type == "Electron":
+
+		def free_mean_path(energy):
+		
+			if energy < cross_section[-1][0]:
+				for pair in cross_section:
+					if (pair[0]-energy) < 1E-2:
+						sigma = pair[1]
+			else:
+				a = 1.25E-14
+				b = 6.72E-1
+				sigma = a/(energy**(-b))
+			
+			print 0.5/(sigma*n)
+			return 0.5/(sigma*n)
+
+	else:
+		
+		free_mean_path = 2.36E-9 # m
+			
+		
 	# Must be very small to be reasonable, according to the free mean path
-	step = 0.00000000001
+	step = 1E-15
 	
 	# Number of ions to be launched
-	ions = 20
+	ions = 1000
 	
 	print "3D Simulation"
-	simulate_collisions3D(step, free_mean_path, electric_field, ions, particle_type)
+	import profile
+	profile.run('simulate_collisions3D(step, free_mean_path, electric_field, ions, particle_type)')
